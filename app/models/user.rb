fa98dea7cf04e1762cde842c0225c1f91028254b
Hasan Ahmed <hasan@hasanme.com>
2015-11-05 17:50:51 +0000

    Add Pins Scaffold

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  # setup accessible (or protected) attributes for your model.
+  #attr_accessible :email, :password, :password_confirmation, :remember_me
+  #attribute_accessible :title, :body
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151102203330) do
+ActiveRecord::Schema.define(version: 20151105140716) do
+
+  create_table "pins", force: :cascade do |t|
+    t.string   "description", limit: 255
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -26,6 +32,7 @@ ActiveRecord::Schema.define(version: 20151102203330) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "name",                   limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
